adcaea2cbb638c836a55a9ba8cd1b80a47041d14
str1ker <gabrismar333@gmail.com>
2023-05-04 16:08:41 -0400

    "Main Blur Container"
    Update: "src/static/css/index.css" - Added styles to blur the main container when expand and collapse the sidebar;
    
    Update: "src/static/js/sidebar.js" - Show the main blur container when click the menu icon;
    
    Update: "src/views/layouts/main.hbs" - Added the main blur container;

--- src/static/js/sidebar.js
+++ src/static/js/sidebar.js
@@ -3,16 +3,19 @@ export default function sidebar() {
     const sidebar = document.getElementById("sidebar");
     const navbar = document.getElementById("navbar");
     const main_container = document.getElementById("main-container");
+    const main_blur_container = document.getElementById("main-blur-container")
 
     let menuIsActive = false;
 
     menu_icon.addEventListener("click", () => {
         if (menuIsActive == false) {
+            main_blur_container.style.display = "block"
             sidebar.style.display = "block";
             navbar.style.gridColumn = "span 1";
             main_container.style.gridColumn = "span 1";
             menuIsActive = true;
         } else {
+            main_blur_container.style.display = "none"
             sidebar.style.display = "none";
             navbar.style.gridColumn = "span 2";
             main_container.style.gridColumn = "span 2";
