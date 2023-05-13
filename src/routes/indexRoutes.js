1217ad29c90f3ad3ed6eee4df6e19176ba9de7e6
str1ker <gabrismar333@gmail.com>
2023-05-12 22:43:03 -0400

    "Logout, Sidebar styles"
    Update: "src/routes/indexRoutes.js" - Formated;
                                          Added route to logout a user session;
    
    Update: "src/static/css/index.css" - Addes styles to better lookoof sidebar;
    
    Update: "src/views/layouts/main.hbs" - Added link to logout a session;

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,36 +1,56 @@
 import { Router } from "express";
 import passport from "../passport/localAuth.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/", (req,res) => {
-    res.render("index")
-})
+router.get("/", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("index");
+    } else {
+        res.redirect("/signup");
+    }
+});
 
-router.get("/signin", (req,res) => {
-    const message = req.session.message
-    delete req.session.message
-    res.render("signin_loggin/signin", { styles: "signInputs", message})
-})
+router.get("/signin", (req, res) => {
+    const message = req.session.message;
+    delete req.session.message;
+    res.render("signin_loggin/signin", { styles: "signInputs", message });
+});
 
-router.post("/signin", passport.authenticate("local-signin", {
-    successRedirect: "/",
-    failureRedirect: "/signin",
-    passReqToCallback: true,
-    keepSessionInfo: true
-}))
+router.post(
+    "/signin",
+    passport.authenticate("local-signin", {
+        successRedirect: "/",
+        failureRedirect: "/signin",
+        passReqToCallback: true,
+        keepSessionInfo: true,
+    })
+);
 
-router.get("/signup", (req,res) => {
-    const message = req.session.message
-    delete req.session.message
-    res.render("signin_loggin/signup", { styles: "signInputs", message })
-})
+router.get("/signup", (req, res) => {
+    const message = req.session.message;
+    delete req.session.message;
+    res.render("signin_loggin/signup", { styles: "signInputs", message });
+});
 
-router.post("/signup", passport.authenticate("local-signup", {
-    successRedirect: "/",
-    failureRedirect: "/signup",
-    passReqToCallback: true,
-    keepSessionInfo: true
-}))
+router.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+        successRedirect: "/",
+        failureRedirect: "/signup",
+        passReqToCallback: true,
+        keepSessionInfo: true,
+    })
+);
 
-export default router
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err)
+        }
+        req.session.message = "Sesion cerrada"
+        res.redirect("/signup")
+    })
+});
+
+export default router;
