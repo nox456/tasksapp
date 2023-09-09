53b0f9339800a79a16940b6c0d13f71257833033
Nox <gabrismar333@gmail.com>
2023-09-08 21:08:59 -0400

    "Changed icons library, Removed css.gg"
    Update: "package.json" - Removed 'css.gg' module;
    
    Update: "src/static/css/index.css" - Removed styles of 'css.gg' icons;
                                         Changed styles of main heade icons;
    
    Update: "src/static/css/profile.css" - Changed styles of change image button to the new icons;
    
    Update: "src/static/js/expandHabits.js" - Changed 'svg' icon when click on habit element;
    
    Update: "src/static/js/expandTasks.js" - Changed 'svg' icon when lick on task element;
    
    Update: "src/static/js/expandTodayElements.js" - Changed 'svg' icon when click on today elements;
    
    Update: "src/views/habits/habitsList.hbs" - Changed icon of add button;
    
    Update: "src/views/layouts/main.hbs" - Removed 'css.gg' CDN;
    
    Update: "src/views/partials/header.hbs" - Changed icons of header;
    
    Update: "src/views/partials/listHabits.hbs" - Changed icons of habits elements;
    
    Update: "src/views/partials/listTasks.hbs" - Changed icons of task elements;
    
    Update: "src/views/partials/searchTaskInput.hbs" - Changed icon of search task input;
    
    Update: "src/views/partials/searchHabitInput.hbs" - Changed icon of search habit input;
    
    Update: "src/views/tasks/taskList.hbs" - Changed icon of add button;
    
    Update: "src/views/users/profile.hbs" - Changed icon of change image button;

--- src/static/js/expandHabits.js
+++ src/static/js/expandHabits.js
@@ -4,8 +4,8 @@ export default function expandHabits() {
     const daysContainer = Array.from(
         document.getElementsByClassName("days-expand")
     );
-    const expandIcon = Array.from(
-        document.getElementsByClassName("expand-icon")
+    const iconContainer = Array.from(
+        document.getElementsByClassName("icon-container")
     );
     const category = Array.from(
         document.getElementsByClassName("category-expand")
@@ -13,27 +13,32 @@ export default function expandHabits() {
     const options = Array.from(
         document.getElementsByClassName("options-expand")
     );
-
     elements.forEach((elem, ind) => {
         elem.addEventListener("click", () => {
-            if (expandIcon[ind].classList.contains("gg-chevron-down")) {
+            if (iconContainer[ind].classList.contains("close")) {
                 category[ind].style.display = "block";
                 options[ind].style.display = "flex";
                 daysContainer[ind].style.display = "flex";
-                expandIcon[ind].classList.replace(
-                    "gg-chevron-down",
-                    "gg-chevron-up"
-                );
-                elem.style.cursor = "default"
+                iconContainer[ind].innerHTML = `
+                    <svg width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M6 9l6 6l6 -6"></path>
+                    </svg>
+                `
+                iconContainer[ind].classList.replace("close", "open")
+                elem.style.cursor = "default";
             } else {
                 category[ind].style.display = "none";
                 options[ind].style.display = "none";
                 daysContainer[ind].style.display = "none";
-                expandIcon[ind].classList.replace(
-                    "gg-chevron-up",
-                    "gg-chevron-down"
-                );
-                elem.style.cursor = "pointer"
+                iconContainer[ind].innerHTML = `
+                    <svg width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M6 15l6 -6l6 6"></path>
+                    </svg>
+                `
+                iconContainer[ind].classList.replace("open", "close")
+                elem.style.cursor = "pointer";
             }
         });
     });
