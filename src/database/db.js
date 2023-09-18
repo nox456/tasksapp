6a1f38801529dd8a0f0e92efdde61d32530a226b
Nox <gabrismar333@gmail.com>
2023-09-18 10:44:44 -0400

    "Created 'dev' script in package.json"
    Update: "package.json";
    
    Update: "src/database/db.js";

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,14 @@
 import pg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const pool = new pg.Pool({
     host: process.env.PGHOST,
-    user: process.env.PGUSER, 
+    user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
-    port: process.env.PGPORT
+    port: process.env.PGPORT,
 });
 
 export default pool;
