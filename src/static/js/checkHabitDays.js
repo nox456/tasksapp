2f0b24a3e2b9f2d92940b011f2a0d943afccf574
Nox <gabrismar333@gmail.com>
2023-09-23 20:16:15 -0400

    "Show today habits in dashboard"
    Update: "src/static/js/checkHabitDays.js" - Used short days names;
    
    Update: "src/views/partials/habits/listHabits.hbs" - Added some styles to show the today habits in dashboard;
    
    Update: "src/views/partials/tasks/listTasks.hbs" - Removed classes from no tasks text in dashboard;

--- src/static/js/checkHabitDays.js
+++ src/static/js/checkHabitDays.js
@@ -2,13 +2,13 @@
 export default function checkHabitDays() {
     const habits_containers = Array.from(document.getElementsByClassName("habit-container"))
     const days_week = {
-        1: "Lunes",
-        2: "Martes",
-        3: "Miercoles",
-        4: "Jueves",
-        5: "Viernes",
-        6: "Sabado",
-        7: "Domingo"
+        1: "L",
+        2: "M",
+        3: "X",
+        4: "J",
+        5: "V",
+        6: "S",
+        7: "D"
     }
     const today = new Date().getDay()
 
